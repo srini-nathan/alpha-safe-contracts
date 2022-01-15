9603c55638073ca0e6170efcaedcb29d69350fb8
James Bond <rodrigoheitie@hotmail.com>
2022-01-14 18:17:07 -0600

    config updates on optimizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,22 +1,37 @@
-require("dotenv").config();
 import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-ethers";
 
+require("dotenv").config();
+
 const RINKEBY_URL = process.env.URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ALCHEMY_URL = process.env.ALCHEMY_URL;
+const ALCHEMY_KEY = process.env.ALCHEMY_KEY;
+
+if (PRIVATE_KEY?.length != 64) {
+  console.error(`Incorrect Private Key!, length should be 64 but it is: ${PRIVATE_KEY?.length}`);
+}
+
 
 module.exports = {
-  solidity: "0.8.3",
+  solidity: {
+    version: "0.8.3",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 800
+      }
+    }
+  },
   networks: {
     //   rinkeby: {
-    //     url: URL,
+    //     url: ALCHEMY_URL,
     //     accounts: [`0x${PRIVATE_KEY}`]
     //   }
     // }, etherscan: {
-    //   apiKey: "WCG58ENQ5WV7RGV9QX3DWTT8ZTMIXTVHJY"
+    // apiKey: ALCHEMY_KEY
     hardhat: {
       forking: {
         url: ALCHEMY_URL
@@ -24,3 +39,4 @@ module.exports = {
     }
   }
 };
+
