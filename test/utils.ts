00d12b6e15312963b3f9e34d10e730b9f25da229
James Bond <rodrigoheitie@hotmail.com>
2022-01-15 14:02:35 -0600

    update erc-20 abi for testing

--- test/utils.ts
+++ test/utils.ts
@@ -17,6 +17,7 @@ export const executorSignature = (_address: string): string => {
 }
 
 export const singletonAbi = ["function setup(address[] calldata _owners,uint256 _threshold)"];
-export const erc20Abi = ["function approve(address spender, uint rawAmount) external returns (bool)", "function balanceOf(address account) external view returns (uint)", "function transfer(address dst, uint rawAmount) external returns (bool)", "function balanceOfUnderlying(address account) external view returns (uint)"];
+export const erc20Abi = ["function approve(address spender, uint rawAmount) external returns (bool)", "function balanceOf(address account) external view returns (uint)", "function transfer(address dst, uint rawAmount) external returns (bool)", "function balanceOfUnderlying(address account) external view returns (uint)", "function borrowBalanceCurrent(address account) external view returns (uint256)"];
 
 export const VERSION = "0.0.1"; // Beta version.
+
