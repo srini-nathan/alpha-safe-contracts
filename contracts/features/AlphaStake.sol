7c07f5a25a9b40dc934b85ee8b8d60b428edcbac
James Bond <rodrigoheitie@hotmail.com>
2022-01-16 18:25:07 -0600

    Staking contract through Rocket pool setup

---
+++ contracts/features/AlphaStake.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.8.0 <0.9.0;
+
+import "../common/SelfAuthorized.sol";
+
+/**
+ * @title AlphaStake - A decentralized Ethereum staking contract, done through Rocket Pool.
+ * @author Rodrigo Herrera I.
+ */
+contract AlphaStake is SelfAuthorized {
+
+}
