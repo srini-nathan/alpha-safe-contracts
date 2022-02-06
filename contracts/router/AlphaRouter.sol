611e4d3a8964ae98d341b2916bcde35af5c04b9b
Rodrigo Herrera Itie <80117772+rodrigoherrerai@users.noreply.github.com>
2022-02-06 15:33:16 +0100

    Update AlphaRouter.sol

--- contracts/router/AlphaRouter.sol
+++ contracts/router/AlphaRouter.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.9;
 
 /**
- * @title Router - A contract that holds all relevant addresses for Alpha Safe.
+ * @title AlphaRouter - A contract that holds all relevant addresses for Alpha Safe.
  * @notice It is safer to have the addresses in an external Solidity contract that providing them
  * as input through the frontend. It is also more decentralized as people can just look to this contract to check that the addresses are correct.
  * We stronly belive that the frontend can be a critical point of failure, that is why we try to reduce the risks as much as possible.
