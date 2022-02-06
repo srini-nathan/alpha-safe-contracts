824966a3e9fcee3ca9802b20a88898911683e226
James Bond <rodrigoheitie@hotmail.com>
2022-02-06 15:32:05 +0100

    init Router

---
+++ contracts/router/AlphaRouter.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity ^0.8.9;
+
+/**
+ * @title Router - A contract that holds all relevant addresses for Alpha Safe.
+ * @notice It is safer to have the addresses in an external Solidity contract that providing them
+ * as input through the frontend. It is also more decentralized as people can just look to this contract to check that the addresses are correct.
+ * We stronly belive that the frontend can be a critical point of failure, that is why we try to reduce the risks as much as possible.
+ * @author Rodrigo Herrera I.
+ */
+contract Router {
+
+}
